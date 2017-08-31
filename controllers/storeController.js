3e7819301e74e59acf5b8395a7aca0260070e798
Ossaija ThankGod <codebugsolved@gmail.com>
2017-08-31 21:59:31 +0100

    added mixin for creating stores, add methods on storeController to persist store data in db and handle request to creating store

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -1,3 +1,17 @@
+'ust strict'
+const mongoose = require('mongoose')
+const Store = mongoose.model('Store')
+
 exports.homePage = (req, res) => {
   res.render('index', {title: 'Home page'})
+}
+
+exports.addStore = (req, res) => {
+  res.render('editStore', { title: 'Add Store' })
+}
+
+exports.createStore = async (req, res) => {
+  const store = new Store(req.body)
+  await store.save()
+  res.redirect('/')
 }
--- models/store/schema.js
+++ models/store/schema.js
@@ -2,7 +2,6 @@
 const mongoose = require('mongoose')
 const slug = require('slug')
 const methods = require('./methods')
-mongoose.Promise = global.Promise
 
 const schema = new mongoose.Schema({
   name: {type: String, trim: true, required: 'Please enter a store name'},
